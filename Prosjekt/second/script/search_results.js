5d70072a721ebc46cc95f58b5109b48ebda5e310
INF112v16-yst003 <yijun@staut.land>
2017-03-28 09:45:29 +0200

    Søkfunksjon i search_result.js er implementert

--- Prosjekt/second/script/search_results.js
+++ Prosjekt/second/script/search_results.js
@@ -3,19 +3,35 @@ function search_for_X() {
 }
 */
 
+function search_for_title(filmTitle, moviesObject){
+	for (movie in moviesObject){
+		if (movie.film_title === filmTitle){
+			return movie;
+		}
+	}
+}
+
 /* Her kan dere implementere en display function som viser resulatetene av søket. For eksempel:
 function display_X() {
 }
 */
 
+function display_movie(movie){
+	
+}
+
 window.onload = function() {
 	query_params = get_query_string_parameters();
 
 	search_results = movies_object;
+
+	var movie;
 	
 	if (query_params.film_title) {
         film_title = document.getElementById("film_title");
 		//Her kan dere for eksempel kalle en søkefunksjon som søker for tittel.
+
+		movie = search_for_title(film_title. search_results);
     }
 	
 	if (query_params.actor) {
