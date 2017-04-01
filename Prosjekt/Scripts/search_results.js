3139e3d42feb7077ec5a087ed7605cb8c953bc3d
INF112v16-yst003 <yijun@staut.land>
2017-04-02 01:09:22 +0200

    Avansert soek

--- Prosjekt/Scripts/search_results.js
+++ Prosjekt/Scripts/search_results.js
@@ -19,47 +19,112 @@ function search_for_title(searchTitle, movieDict){
 /* Her kan dere implementere en display function som viser resulatetene av søket. For eksempel:
 function display_X() {
 }
+
+Se template_engine.js
 */
 
-var searchResults = [
-]
+function advanced_search(title, actor, director, genre, country, movieDict, genreDict){
+	results = [];
+ 
+	for (key in movieDict){
+		if (key in genreDict) {
+			movieDict[key].genre = genreDict[key]
+		}
+	}
+
+	for (key in movieDict){
+		results.push(movieDict[key]);
+	}
+
+	if (title) {
+		results = results.filter(function(movie) {
+			if (movie.otitle) {
+				return movie.otitle.toLowerCase().match(title.toLowerCase())
+			} else {
+				return false;
+			}
+		})
+	}
+
+	if (actor) {
+		results = results.filter(function(movie) {
+			if (movie.folk) {
+				return movie.folk.toLowerCase().match(actor.toLowerCase());
+			} else {
+				return false;
+			}
+		})
+	}
+
+	if (director) {
+		results = results.filter(function(movie) {
+			if (movie.dir) {
+				return movie.dir.toLowerCase().match(director.toLowerCase());
+			} else {
+				return false;
+			}
+		})
+	}
+
+	if (genre) {
+		results = results.filter(function(movie) {
+			if (movie.genre) {
+				return movie.genre.indexOf(genre.toLowerCase()) > -1;
+			} else {
+				return false;
+			}
+		})
+	}
+
+	if (country) {
+		results = results.filter(function(movie) {
+			if (movie.country) { 
+			return movie.country.toLowerCase().match(country.toLowerCase());
+			} else {
+				return false;
+			}
+		})
+	}
+
+	return results;
+
+}
+
+var searchResults = []
 
 window.onload = function() {
-	query_params = get_query_string_parameters();
-
-	search_results = movies_object;
+	qp = get_query_string_parameters();
 
 	// denne variablen lagres moviedetaljer fra søkefunksjonen.
-	var foundMovies;
 	
-	if (query_params.film_title) {
-     	film_title = document.getElementById("film_title");
-		//Her kan dere for eksempel kalle en søkefunksjon som søker for tittel.
+	//if (query_params.film_title) {
+    // 	film_title = document.getElementById("film_title");
+    //}
+	//
+	//if (query_params.actor) {
+    //    actor = document.getElementById("actor");
+	//	  actor.innerHTML = query_params.actor; 
+    //}
+	//
+	//if (query_params.director) {
+	//	director = document.getElementById("director");
+	//	director.innerHTML = query_params.director; 
+    //}
+	//
+	//if (query_params.genre) {
+    //    genre = document.getElementById("genre");
+	//	  genre.innerHTML = query_params.genre; 
+    //}
+	//
+	//if (query_params.country) {
+    //    country = document.getElementById("country");
+	//	  country.innerHTML = query_params.country; 
+    //}
 
-		foundMovies = search_for_title(query_params.film_title, search_results);
-    }
-	
-	if (query_params.actor) {
-        actor = document.getElementById("actor");
-		actor.innerHTML = query_params.actor;
-    }
-	
-	if (query_params.director) {
-		director = document.getElementById("director");
-		director.innerHTML = query_params.director;
-    }
-	
-	if (query_params.genre) {
-        genre = document.getElementById("genre");
-		genre.innerHTML = query_params.genre;
-    }
-	
-	if (query_params.country) {
-        country = document.getElementById("country");
-		country.innerHTML = query_params.country;
-    }
-	
+
+	searchResults = advanced_search(qp.film_title, qp.actor, qp.director, qp.genre, qp.country, movies_object, genres_object)
+
 	//Her kan dere for eksempel kalle en (display) funksjon som viser søkeresultater 
-	searchResults = foundMovies;
+
 	display();
 }
